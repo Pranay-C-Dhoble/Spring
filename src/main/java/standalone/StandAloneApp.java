341183ff534d04db6b20eb9d2ff9cd9cb31aa294
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2024-01-13 14:17:19 +0530

    StandAlone

--- src/main/java/standalone/StandAloneApp.java
+++ src/main/java/standalone/StandAloneApp.java
@@ -10,4 +10,16 @@ public class StandAloneApp {
         Person person = context.getBean("person1",Person.class);
         System.out.println(person);
     }
+
+    //Spring Stanalone Collection Injection
+    //it is used to inject the collection values into the bean class
+    //Spring provides the following collection types
+    //1. List
+    //2. Set
+    //3. Map
+    //4. Properties  //properties means key value pair
+
+
+    //Add Schema in the XML file
+    //xmlns:util="http://www.springframework.org/schema/util"
 }
